fc53b5305383259925462c0c0852d71c0942e99b
Pedro Tonini <51178173+ptonini@users.noreply.github.com>
2022-12-03 22:23:08 -0300

    chore: updated dependency sources

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+locals {
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "sts:AssumeRole"
+        ],
+        Resource = var.assume_role_arns
+      }
+    ]
+  })
+}
+
 resource "aws_iam_policy" "this" {
   name = var.name
   policy = length(var.assume_role_arns) > 0 ? local.assume_role_policy : var.policy
